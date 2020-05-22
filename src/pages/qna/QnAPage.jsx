0de100ef6b1597cbfa4c19c6b1746c9d7f18f064
Daryll Wong <daryll_wong@hotmail.com>
2020-05-22 22:59:21 +0800

    demo darkgreenoverlay on QnA

--- src/pages/qna/QnAPage.jsx
+++ src/pages/qna/QnAPage.jsx
@@ -1,10 +1,24 @@
 import React from 'react';
+import DarkGreenOverlay from '../../components/DarkGreenOverlay';
 
 
 function QnAPage(){
     return(
+        <>
         <p>QnA Page</p>
+        <DarkGreenOverlay>
+            <div>sdasd</div>
+            <div>sdasd</div>
+            <div>sdasd</div>
+            <div>sdasd</div>
+            <div>sdasd</div>
+            <div>sdasd</div>
+        </DarkGreenOverlay>
+        </>
     )
 }
 
 export default QnAPage;
+
+
+
