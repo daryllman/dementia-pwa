0b7abbc1eec55365e9d456e0984c9cb426de2c51
Daryll Wong <daryll_wong@hotmail.com>
2020-05-23 01:55:37 +0800

    add ThreadBox component. Dynamic mapping

---
+++ src/components/ThreadBox.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import styled from 'styled-components';
+import TextBox from './TextBox';
+import Pill from './Pill';
+import {CommentNLikeWrapper} from './icons/Comment-N-Like-Button';
+
+const LapsedText = styled.div`
+  color: var(--primary-green-color);
+  font-size: 11px;
+`
+
+const TopContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  margin-bottom: 12px;
+`
+
+
+//Inputs needed: Trait name, Number of hours ago (hardcoded for now), Main content text, Number of comments, Number of likes
+//i.e.: trait, hoursAgo, question, commentsNum, likesNum
+
+function ThreadBox({trait, hoursAgo, question, commentsNum, likesNum}){
+    return(
+        <>
+        <TextBox>
+            <TopContainer>
+            <Pill>{trait}</Pill> <LapsedText>{hoursAgo}</LapsedText>
+            </TopContainer>
+                {question}
+            <CommentNLikeWrapper commentsNum={commentsNum} likesNum={likesNum} />
+        </TextBox>
+        </>
+    )
+}
+
+export default ThreadBox;
--- src/pages/qna/QnAPage.jsx
+++ src/pages/qna/QnAPage.jsx
@@ -5,30 +5,23 @@ import PageTitle from '../../components/PageTitle'
 import TextBox from '../../components/TextBox'
 import Pill from '../../components/Pill'
 import {CommentButton, CommentWrapper, LikeButton, LikeWrapper,CommentNLikeWrapper} from '../../components/icons/Comment-N-Like-Button'
+import ThreadBox from '../../components/ThreadBox';
+
+import {qnaData} from '../../mockData';
 
 function QnAPage(){
+    //const latestQuestions = qnaData.latestQuestions;
     return(
         <>
         <BackButton/>
         <PageTitle>Q&A Page</PageTitle>
         <DarkGreenOverlay>
-            <TextBox>
-                <Pill>Wandering</Pill>
-                sadasds sdasd as das dasd asd asd asd asd asd asd asd asd asd asd asd asd asd asd asd asd asd asd as dasd as dasd as ds dasdsdsds dsdsdsdsd sdsdsd asd asd asd sd sd s dasd asd asd asd asd 
-                 <CommentNLikeWrapper/>
-            </TextBox>
-            <TextBox>
-                sadasds sdasd as das dasd asd asd asd asd asd asd asd asd asd asd asd asd
-            </TextBox>
-            <TextBox>
-                sadasds sdasd as das dasd asd asd asd asd asd asd asd asd asd asd  d asd asd asd asd asd asdd asd asd asd asd  d asd asd asd asd asd asdd asd asd asd asd  d asd asd asd asd asd asd  as das dasd asd asd asd asd asd asd asd asd asd asd asd asd asd asd asd asd asd asd as dasd as dasd as ds dasdsdsds dsdsdsdsd sdsdsd asd asd asd sd sd s dasd aasd asd  as das dasd asd asd asd asd asd asd asd asd asd asd asd asd asd asd asd asd asd asd as dasd as dasd as ds dasdsdsds dsdsdsdsd sdsdsd asd asd asd sd sd s dasd asd asd asd asd  
-            </TextBox>
-            <div>sdasd</div>
-            <div>sdasd</div>
-            <div>sdasd</div>
-            <div>sdasd</div>
-            <div>sdasd</div>
-            <div>sdasd</div>
+
+            {qnaData.latestQuestions.map(( item )=>( // attributes: trait, hoursAgo, question, commentsNum, likesNum
+                <ThreadBox trait={item.trait}hoursAgo={item.hoursAgo} question={item.question} commentsNum={item.commentsNum} likesNum={item.likesNum} />
+            )
+            )}
+            
         </DarkGreenOverlay>
         </>
     )
