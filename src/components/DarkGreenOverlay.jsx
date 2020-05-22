a2ae31b78d4a3ac96b4bbe3b343aed44f73337a5
Daryll Wong <daryll_wong@hotmail.com>
2020-05-22 22:58:36 +0800

    add darkgreen overlay

---
+++ src/components/DarkGreenOverlay.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import {Link} from 'react-router-dom'
+import styled from 'styled-components'
+
+const DarkGreenContainer = styled.div`
+  background-color: var(--secondary-green-color);
+  min-height: 95vh;
+  border-radius: 40px 40px 0px 0px;
+`
+
+const TopGap = styled.div`
+  display: block;
+  height:30px;
+`
+
+
+function DarkGreenOverlay({children}){
+    return(
+    <DarkGreenContainer>
+      <TopGap/>
+      {children}
+    </DarkGreenContainer>
+    )
+}
+
+export default DarkGreenOverlay;
