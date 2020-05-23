04892aafb672e6ccfd028dd1f0e0feee443c56e0
Daryll Wong <daryll_wong@hotmail.com>
2020-05-23 22:14:24 +0800

    fix to sync manual input path and selected bottom nav button

--- src/components/BottomNavBar.jsx
+++ src/components/BottomNavBar.jsx
@@ -19,29 +19,33 @@ const useStyles = makeStyles({
       width: '100vw',
       position: 'fixed',
       bottom: '0',
-      background:'F7F7F7',
+      background:'F7F7F7', //background color of whole nav bar
+      color: 'green',
+      '&$selected': {
+        color: 'red', //to fix...
+      },
     },
-    selected:{
-        color:'black',
-        background:'black'
+    selected: {
+        //color:'black', // unfocused color
     }
   });
 
 function BottomNavBar(){
     const history = useHistory();
-
     const classes = useStyles();
-    const [value, setValue] = React.useState('home');
+    const pathname = window.location.pathname; // in case user visits the path directly. The BottomNavBar is able to follow suit.
+    const [value, setValue] = React.useState(pathname);
     const handleChange = (event, newValue) => {
-      setValue(newValue);
+        //console.log(event); //complex object returned
+        setValue(newValue);
     };
 
     return (
-        <BottomNavigation value={value} onChange={handleChange} showLabels={true} className={classes.root} >
-          <BottomNavigationAction label="home" value="home" icon={<HomeIcon />} onClick={()=>history.push("/")}/>
-          <BottomNavigationAction label="resources" value="resources" icon={<ResourcesIcon /> } onClick={()=>history.push("/resources")}/>                
-          <BottomNavigationAction label="Q&A" value="qna" icon={<QnAIcon />} onClick={()=>history.push("/qna")}/>
-          <BottomNavigationAction label="profile" value="profile" icon={<ProfileIcon />} onClick={()=>history.push("/profile")}/>
+        <BottomNavigation value={value} onChange={handleChange} showLabels={true} classes={{ root: classes.root, selected: classes.selected }}>
+          <BottomNavigationAction label="home" value="/" icon={<HomeIcon />} onClick={()=>console.log('Home clicked')} component={Link} to='/'  />
+          <BottomNavigationAction label="resources" value="/resources" icon={<ResourcesIcon /> } onClick={()=>console.log('Resources clicked')} component={Link} to='/resources'/>                
+          <BottomNavigationAction label="Q&A" value="/qna" icon={<QnAIcon />} onClick={()=>console.log('Q&A clicked')} component={Link} to='/qna'/>
+          <BottomNavigationAction label="profile" value="/profile" icon={<ProfileIcon />} onClick={()=>console.log('Profile clicked')} component={Link} to='/profile'/>
         </BottomNavigation>
       );
 }
